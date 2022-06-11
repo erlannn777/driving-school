f83165900c1f55d90ace4223bedf6ecfabf59c55
erlannn777 <erlank123456789@gmail.com>
2022-06-11 17:34:25 +0600

    test

--- src/pages/test/Decode.tsx
+++
@@ -1,6 +0,0 @@
-const decode = (html: any) => {
-  var txt = document.createElement("textarea");
-  txt.innerHTML = html;
-  return txt.value;
-};
-export default decode;
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,117 +1,112 @@
-import React, { useState, useEffect } from "react";
-import "./styles.css";
-import Question from "./Question";
-import { useTest } from "../../store/courses/hooks";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useAppDispatch } from "../../store";
 import { fetchTest } from "../../store/courses";
-import { useParams } from "react-router-dom";
-import API from "../../constants/api";
+import { useTest } from "../../store/courses/hooks";
+import "./styles.css";
+
+const questions = [
+  {
+    questionText: "What is the capital of France?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Paris", isCorrect: true },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Who is CEO of Tesla?",
+    answerOptions: [
+      { answerText: "Jeff Bezos", isCorrect: false },
+      { answerText: "Elon Musk", isCorrect: true },
+      { answerText: "Bill Gates", isCorrect: false },
+      { answerText: "Tony Stark", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The iPhone was created by which company?",
+    answerOptions: [
+      { answerText: "Apple", isCorrect: true },
+      { answerText: "Intel", isCorrect: false },
+      { answerText: "Amazon", isCorrect: false },
+      { answerText: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How many Harry Potter books are there?",
+    answerOptions: [
+      { answerText: "1", isCorrect: false },
+      { answerText: "4", isCorrect: false },
+      { answerText: "6", isCorrect: false },
+      { answerText: "7", isCorrect: true },
+    ],
+  },
+];
 
 const Test = () => {
   const dispatch = useAppDispatch();
   const params = useParams();
   const test: any = useTest();
-  const [questions, setQuestions] = useState<[]>();
-  const [activeQuestion, setActiveQuestion] = useState(0);
-  const [totalQuestions, setTotalQuestions] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     if (!params.id) return;
     dispatch(fetchTest(Number(params.id)) as any);
   }, [dispatch, params.id]);
 
-  useEffect(() => {
-    if (!test || !test.questions) return;
-    setTotalQuestions(test.questions?.length);
-    setQuestions(test.questions);
-  }, [test]);
-
-  const proceed = (e: any) => {
+  const handleAnswerOptionClick = (e: any) => {
     e.preventDefault();
-    setActiveQuestion(activeQuestion + 1);
-  };
-  const back = (e: any) => {
-    e.preventDefault();
-    setActiveQuestion(activeQuestion - 1);
-  };
+    const checkedValues = Array.from(e.target.answer).map((el: any) => [
+      el.id,
+      el.checked,
+    ]);
+    console.log(e.target.value);
 
-  const addAnswers = (formData: any) => {
-    try {
-      const values: any = {};
-      for (var pair of formData) {
-        var key = pair[0];
-        var value = pair[1];
-        console.log(pair);
-        if (values[key]) {
-          if (!(values[key] instanceof Array)) {
-            values[key] = new Array(values[key]);
-          }
-          values[key].push(value);
-        } else {
-          values[key] = value;
-        }
-      }
-      // API.post(`/Test/SendResult?themeId=${params.id}`, {
-      //   questionId: "",
-      //   questionText: "",
-      //   answers: [
-      //     {
-      //       answerId: "",
-      //       textAnswer: "",
-      //     },
-      //   ],
-      // });
-    } catch (e) {
-      console.log(e);
+    console.log(Object.fromEntries(checkedValues));
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
     }
   };
-
-  const submit = (e: any) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    addAnswers(formData);
-    const erlan = formData.entries();
-    console.log(e);
-
-    // erlan.map((item: any) => {
-    //   console.log(item);
-    // });
-  };
+  console.log(test);
 
   return (
-    <div className="App">
-      {questions ? (
-        <form onSubmit={submit}>
-          {questions.map((data: any, i: any) => {
-            const allAnswers: any = [];
-            data.answers.forEach((answer: any, i: any) => {
-              allAnswers.push({
-                id: answer.answerId,
-                answer: answer.textAnswer,
-              });
-            });
-            return (
-              <Question
-                key={i}
-                questionText={data.questionText}
-                questionId={data.id}
-                visible={i === activeQuestion}
-                answers={allAnswers}
-              />
-            );
-          })}
-          {activeQuestion > 0 && activeQuestion + 1 !== totalQuestions && (
-            <button onClick={back}>Previous</button>
-          )}
-          {activeQuestion + 1 < totalQuestions &&
-            activeQuestion + 1 !== totalQuestions && (
-              <button onClick={proceed}>Next</button>
-            )}
-
-          {activeQuestion + 1 === totalQuestions && (
-            <button type="submit">Submit</button>
-          )}
-        </form>
+    <div className="appp">
+      {test.questions ? (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/
+              {test.questions.length}
+            </div>
+            <div className="question-text">
+              {test.questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            <form onSubmit={handleAnswerOptionClick}>
+              {test.questions[currentQuestion].answers.map(
+                (answerOption: any, id: any) => (
+                  <label key={id}>
+                    {answerOption.textAnswer}
+                    <input
+                      type="checkbox"
+                      name="answer"
+                      id={answerOption.textAnswer}
+                    />
+                  </label>
+                )
+              )}
+              <button type="submit">Next</button>
+            </form>
+          </div>
+        </>
       ) : (
         <span>Теста нет</span>
       )}
