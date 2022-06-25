1060ef1c6feb4f649296d58f0593d14ac6bb00fa
Abdurasul <ahmedovabdurasul19@gmail.com>
2022-06-25 15:06:34 +0600

    menu btn

--- src/pages/reports/reports.tsx
+++ src/pages/reports/reports.tsx
@@ -14,32 +14,47 @@ const Reports = () => {
         console.log(obj.get("href"))
         dispatch(getAllGrades(href) as any)
     }, [])
-    const allGrades:string = {name}
     return (
         <div className='container'>
             <div className='flex items-center justify-center'>
-                <div className='w-2/3'>
-                    <table className="table-fixed">
-                        <h2 className='font-bold py-2'>{allGrades.name}</h2>
-                        <tbody>
-                        <tr>
-                            <td className='mb-2'>
-                                <p>{el.nameTheme}</p>
-                                <div className='flex items-center justify-between my-2 w-1/2'>
-                                    <h1>Progress</h1>
-                                    <h1>{el.progress}%</h1>
-                                </div>
-                                <div className="bg-gray-200 h-1 mb-6 w-1/2">
-                                    <div
-                                        className="bg-green-500 h-1"
-                                        style={{width: `${el.progress}%`}}
-                                    />
-                                </div>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+                {
+                    //@ts-ignore
+                    allGrades.length > 0 ? (
+                        <div>
+                            {
+                                //@ts-ignore
+                                allGrades.map((el: { id, nameCourse, grades: [] }) => (
+                                    <div key={el.id} className='w-2/3'>
+                                        <table className="table-fixed" key={el.id}>
+                                            <h2 className='font-bold py-2'>{el.nameCourse}</h2>
+                                            {
+                                                //@ts-ignore
+                                                el.grades.map((el: { id, nameTheme, progress }) => (
+                                                    <tbody key={el.id}>
+                                                    <tr>
+                                                        <td className='mb-2'>
+                                                            <p>{el.nameTheme}</p>
+                                                            <div className='flex items-center justify-between my-2 w-1/2'>
+                                                                <h1>Progress</h1>
+                                                                <h1>{el.progress}%</h1>
+                                                            </div>
+                                                            <div className="bg-gray-200 h-1 mb-6 w-1/2">
+                                                                <div
+                                                                    className="bg-green-500 h-1"
+                                                                    style={{ width: `${el.progress}%` }}
+                                                                />
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                ))
+                                            }
+                                        </table>
+                                    </div>
+                                ))}
+                        </div>
+                    ) : (null)
+                }
             </div>
         </div>
     );
